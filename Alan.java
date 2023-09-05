7ae8592a494efb4cf17d62c0cbae7f7d6702e127
AlperenSagutdev <140455296+AlperenSagutdev@users.noreply.github.com>
2023-09-05 15:13:30 +0300

    Add files via upload

---
+++ Alan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Alan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Double a,b,c;
+        System.out.println("A kenarı için değer giriniz: ");
+        a = input.nextDouble();
+
+        System.out.println("B Kenarı için değer giriniz: ");
+        b = input.nextDouble();
+
+        System.out.println("C Kenarı için değer giriniz");
+        c = input.nextDouble();
+
+        double u = (a+b+c)/2;
+        double alan = Math.sqrt(u * (u-a) * (u-b) * (u-c) );
+        System.out.println("Üçgenin alanı : " +alan );
+
+    }
+}
